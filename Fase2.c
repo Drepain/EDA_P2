02176a63c0dc5dbae95e2e28737378a7cfbf3816
Joca <jocaschool@gmail.com>
2024-05-13 18:27:43 +0100

    added basic InsertNode function, Insert Connection is unfinished

--- Fase2.c
+++ Fase2.c
@@ -9,7 +9,34 @@ typedef struct Connection {
 typedef struct Graph {
   int num;
   Connection *connections;
-  struct GR *next;
+  struct Graph *next;
 } GR;
 
-int main() { return 0; }
+GR *InsertNode(GR *matrix, int number) {
+  GR *new = (GR *)malloc(sizeof(GR));
+
+  if (new == NULL) {
+    perror("error allocating memory");
+    return matrix;
+  }
+
+  new->next = matrix;
+  new->num = number;
+  new->connections = NULL;
+
+  return new;
+}
+
+/*void InsertConnection(GR *matrix, int ID, int link) {
+  Connection *new = (Connection *)malloc(sizeof(Connection));
+  if (new == NULL) {
+    perror("error allocating memory");
+    return;
+  }
+}*/
+
+int main() {
+  GR *matrix = NULL;
+
+  return 0;
+}
